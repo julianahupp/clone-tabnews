f96f4422df3e8725ff4ddf03d5602e5ed30dc254
Juliana Hupp <166019310+julianahupp@users.noreply.github.com>
2024-04-23 17:34:02 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Rafa, te amo! Se você me ama da uma risadinha! Hahahaha</h1>
+    return <h1>Rafa, te amo! Se você me ama da uma risadinha!😜 Hahahaha</h1>
 }
 
 export default Home;
